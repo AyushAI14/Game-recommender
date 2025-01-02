5e6fbebda81d1b4c766c911d6b10b0f5907d743a
AyushAI14 <ayushaiml14@gmail.com>
2025-01-02 13:53:49 +0530

    Completed making working home page

---
+++ Webapp/app.py
@@ -0,0 +1,31 @@
+from flask import Flask,render_template,request
+import pickle
+
+app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
+
+with open('model/homedf.pkl', 'rb') as file:
+    homedf = pickle.load(file)
+with open('model/Maindf.pkl', 'rb') as file:
+    Maindf = pickle.load(file)
+
+@app.route('/')
+def home():
+    return render_template("home.html",
+                           title = list(homedf["title"].values),
+                           image = list(homedf["thumbnail"].values),
+                           short_description = list(homedf["short_description"].values),
+                           game_url = list(homedf["game_url"].values),
+                           genre = list(homedf["genre"].values),
+                           platform = list(homedf["platform"].values),
+                           publisher = list(homedf["publisher"].values),
+                           developer = list(homedf["developer"].values),
+                           release_date = list(homedf["release_date"].values),
+                           freetogame_profile_url = list(homedf["freetogame_profile_url"].values),
+                           )
+
+app.route('/genre')
+def genre():
+    pass
+
+if __name__ == "__main__":
+    app.run(debug=True)
